94c58218ad7b1321ff8cc351bf9e0269407d17b3
Egaliciar <tnt_code@hotmail.com>
2017-11-08 20:28:44 -0600

    Notas de como usar el serializer SIN modelo

---
+++ modulos/artistas/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from modulos.artistas.models import Artista
+
+"""
+Se deben usar cuando no se deba guardar ese resultado y se ocupe algo para validar cosas
+ej endpoint que pase precio y descuento y al final debe regresar el precio con decuento para evitar casteos y demas 
+creamos el serializer especifico que pase el decimal fields para el precio y el decuento y al final el serializer lo debe convertir a decimal y validar que haya casteo
+
+
+class ArtistaSerializer(serializers.Serializer):
+    name = models.CharField(max_length=length, ${blank=True, null=True})
+    .
+    .
+    .
+"""
+class ArtistaSerializer(serializers.Serializer):
+    model=Artista
+
+
