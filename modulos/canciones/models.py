49ea6c8d9790daed1472dfa8d3831aba6d3f0c9b
Egaliciar <tnt_code@hotmail.com>
2017-11-08 19:47:57 -0600

    generamos el modelo de la cancion

--- modulos/canciones/models.py
+++ modulos/canciones/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+import uuid
+from modulos.albumes.models import Album
 # Create your models here.
+
+
+class Cancion(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    nombre = models.CharField(max_length=100)
+    duracion = models.PositiveIntegerField()
+    url_youtube = models.URLField()
+    album = models.ForeignKey(Album, on_delete=models.CASCADE,related_name='fk_canciones')
+    raiting = models.DecimalField(max_digits=2, decimal_places=1, default=0)
+
+    def __str__(self):
+        return self.nombre
+
+    def __unicode__(self):
+        return
