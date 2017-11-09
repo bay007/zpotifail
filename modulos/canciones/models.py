7b3424f0f3cebb40e1ed8ff2a03c6071e6169b3d
Egaliciar <tnt_code@hotmail.com>
2017-11-08 20:05:23 -0600

    cambio de related name

--- modulos/canciones/models.py
+++ modulos/canciones/models.py
@@ -9,7 +9,7 @@ class Cancion(models.Model):
     nombre = models.CharField(max_length=100)
     duracion = models.PositiveIntegerField()
     url_youtube = models.URLField()
-    album = models.ForeignKey(Album, on_delete=models.CASCADE,related_name='fk_canciones')
+    album = models.ForeignKey(Album, on_delete=models.CASCADE,related_name='canciones')
     raiting = models.DecimalField(max_digits=2, decimal_places=1, default=0)
 
     def __str__(self):
