9a4f8503c64ff5b847d922255eab3ac7884ace5a
Egaliciar <tnt_code@hotmail.com>
2017-11-08 19:47:41 -0600

    Se crea el admin de las canciones

--- modulos/canciones/admin.py
+++ modulos/canciones/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from modulos.canciones.models import Cancion
 # Register your models here.
+
+class CancionesAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+
+admin.site.register(Cancion, CancionesAdmin)
