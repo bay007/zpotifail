5a404777779e04384a0000570949cbfd5dd2e84f
Egaliciar <tnt_code@hotmail.com>
2017-11-09 21:35:29 -0600

    se debe poner atencion a lavida si ta se genero todo el crud

--- modulos/albumes/serializers.py
+++ modulos/albumes/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 from modulos.albumes.models import Album
+from modulos.canciones.models import Cancion
 
+# Se debe especificar que es de tipo MODEL, si no la vista retornara vacio
 
-#Se debe especificar que es de tipo MODEL, si no la vista retornara vacio
 class AlbumModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Album
         fields = ('__all__')
+
+
+class AlbumesCancionesSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Cancion
+        fields = ('id','nombre','canciones',)
