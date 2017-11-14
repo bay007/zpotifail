a9123c914a7c55a07ca136e58857aa8b606c28af
Egaliciar <tnt_code@hotmail.com>
2017-11-13 20:32:44 -0600

    agregar el admin a django

--- modulos/usuarios/admin.py
+++ modulos/usuarios/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
-
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin as UA
+from modulos.usuarios.models import Usuario
+class UserAdmin(UA):
+    fieldsets = (
+        ('Datos de Cuenta', {
+            'fields': ('email', 'password', 'is_active', 'is_staff', 'last_login')
+        }), ('Datos personales', {
+            'fields': ('nombre', 'apaterno', 'amaterno', 'numero_celular', 'genero')
+        }),
+        ('Grupos', {
+            'fields': ('groups',)
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'groups'),
+        }),
+    )
+    def username(self, instance):
+        return instance.email
+    def user_first_name(self, instance):
+        return instance.nombre
+    def user_last_name(self, instance):
+        return instance.apaterno
+    list_display = ('email', 'user_first_name', 'user_last_name', 'is_active')
+    ordering = ('email',)
+class ApplicationAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Usuario, UserAdmin)
